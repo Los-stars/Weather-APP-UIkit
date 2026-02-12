c1b8658e0ea61fff23c06dd0786eab1792fdfd52
Amir <bagasharov10@gmail.com>
2026-02-12 19:37:44 +0500

    Initial Commit

---
+++ Weather APP UIkitTests/Weather_APP_UIkitTests.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_APP_UIkitTests.swift
+//  Weather APP UIkitTests
+//
+//  Created by Amir on 12.02.2026.
+//
+
+import Testing
+@testable import Weather_APP_UIkit
+
+struct Weather_APP_UIkitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Weather APP UIkitUITests/Weather_APP_UIkitUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Weather_APP_UIkitUITestsLaunchTests.swift
+//  Weather APP UIkitUITests
+//
+//  Created by Amir on 12.02.2026.
+//
+
+import XCTest
+
+final class Weather_APP_UIkitUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
