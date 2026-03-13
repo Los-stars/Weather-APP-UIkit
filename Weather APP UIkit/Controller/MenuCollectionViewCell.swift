268ef41cdcdd4951886e288b59e1ff543a0ebda2
Amir <bagasharov10@gmail.com>
2026-03-13 11:03:15 +0800

    before adding collectionView

---
+++ Weather APP UIkit/Controller/MenuCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  MenuCollectionViewCell.swift
+//  Weather APP UIkit
+//
+//  Created by Amir on 13.03.2026.
+//
+
+import UIKit
+
+class MenuCollectionViewCell: UICollectionViewCell {
+    
+}
